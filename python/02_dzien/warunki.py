d8fc6c6c5ee6a591f4f8beaee10b5746b131395d
Barbara Michalak <mfmichal@gmail.com>
2019-02-25 20:31:59 +0100

    Warunki Wypisanie zmiennych i podstawowych typow danych

---
+++ python/02_dzien/warunki.py
@@ -0,0 +1,19 @@
+liczbacalk = 1
+liczbazmien = 1.3
+stringi = "Ala"
+logika = True # george boole
+zadna = None
+listy = [liczbacalk, liczbazmien, stringi]
+pusta_lista = []
+len(pusta_lista)
+
+print(liczbacalk)
+print(liczbazmien)
+print(stringi)
+print(logika)
+print(zadna)
+print(listy)
+
+
+
+
