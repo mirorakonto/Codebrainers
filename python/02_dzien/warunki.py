6455b02f0a5f83b69fee8d5bbfb25430966e79b8
Barbara Michalak <mfmichal@gmail.com>
2019-02-27 18:20:26 +0100

    bylw co

--- python/02_dzien/warunki.py
+++ python/02_dzien/warunki.py
@@ -14,6 +14,19 @@ print(logika)
 print(zadna)
 print(listy)
 
+if zmienna1 == 3:
+    print(3)
+elif zmienna1 == 4:
+    print(4)
+elif zmienna1 == 5:
+    print(5)
+elif zmienna1 == "ciąg":
+    print("ciąg")
+else:
+    print("Nie miałeś racji")
+
+    
+
 
 
 
