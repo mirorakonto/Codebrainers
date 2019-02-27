9f856ac7fc48e6a7783dba0471929db718f738ad
Barbara Michalak <mfmichal@gmail.com>
2019-02-27 20:26:57 +0100

    Petle

---
+++ python/03_dzien/petle.py
@@ -0,0 +1,20 @@
+lista = ["B", 1, [1,2,3], True, None]
+
+print("Przed petla")
+
+for znak in lista: #zmienna przyjmuje kolejne wartosci
+    print(znak)
+
+print("Po petli")
+
+for liczba in range(21):
+    print(str(liczba)+ ": " + str(liczba**2) + ", " + str(liczba**0.5))
+
+
+
+
+
+
+
+
+
