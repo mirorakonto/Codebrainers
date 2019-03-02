c49d9ab640d90df8b3388b43491054d593bd5c44
Barbara Michalak <mfmichal@gmail.com>
2019-03-02 10:16:36 +0100

    Petla ktora szuka w liscie i zwraca element z indeksem.

---
+++ python/04_dzien/wyszukiwanie_w_liscie.py
@@ -0,0 +1,28 @@
+uczestnik = 'Basia'
+
+lista = ['Piotr', 'Aga', 'Basia']
+
+for zmienna in lista:
+    if zmienna == uczestnik:
+        print('zmienna szukana jest', uczestnik)
+    else:
+        print ('nie znajduje się na liście', uczestnik)
+
+        #`1234567890-=`
+        #~!@#$%^&*()_+
+        #{}[]
+        #:"|";'\,./<>?
+
+if uczestnik in lista:
+    print ('zmienna szukana', uczestnik, 'nie znajduje się na liście')
+
+for pozycja, uczen in enumerate(lista):
+    print (pozycja, ': ', uczen)
+
+print ('********', uczen)
+
+for pozycja, uczen in enumerate(lista):
+    if uczen == uczestnik:
+        print (pozycja, ': ', uczen)
+
+print ('********', uczen)  
